e3645c1aacccaf498fe35408f69c80d5916454c7
appsaf2k <appsaf2k@gmail.com>
2022-07-10 17:02:10 +0300

    Initial Commit

---
+++ MainRegistration/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  MainRegistration
+//
+//  Created by @andreev2k on 10.07.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MainRegistration/MainRegistrationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainRegistrationApp.swift
+//  MainRegistration
+//
+//  Created by @andreev2k on 10.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct MainRegistrationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
